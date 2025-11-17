5a8938eb434a2a21cab08f74c3dd33e291fb70ea
IVANAGUSS12 <ivanaguss12@gmail.com>
2025-11-17 08:35:09 -0300

    Update ALLOWED_HOSTS to include additional IP

--- panel_ova/settings.py
+++ panel_ova/settings.py
@@ -8,8 +8,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'insecure-key')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "10.1.45.64",
+]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
